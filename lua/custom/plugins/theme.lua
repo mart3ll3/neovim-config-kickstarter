582746a167c959f2d07dfd966b153d5b3f791fb1
Juraj Rajnoha <juraj.rajnoha@revalizesoftware.com>
2023-11-07 20:41:14 +0100

    set default theme to gruvbox dark

--- lua/custom/plugins/theme.lua
+++ lua/custom/plugins/theme.lua
@@ -1,5 +1,9 @@
 -- Themery block
 -- This block will be replaced by Themery.
-vim.cmd("colorscheme catppuccin-macchiato")
-vim.g.theme_id = 12
+-- All this block will be executed before apply "set colorscheme"
+vim.opt.background = "dark"
+
+
+vim.cmd("colorscheme gruvbox")
+vim.g.theme_id = 7
 -- end themery block
