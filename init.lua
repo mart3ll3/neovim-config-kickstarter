c961ca7cb227ed1187b7fcb9d85d545a5afbd2c3
Juraj Rajnoha <juraj.rajnoha@revalizesoftware.com>
2024-04-23 20:12:22 +0200

    added back context highlighting for indent blankline

--- init.lua
+++ init.lua
@@ -1021,78 +1021,38 @@ cmp.setup {
     { name = 'buffer',  keyword_length = 3 },
   },
 }
--- -- indent blank line
--- local highlight = {
--- "RainbowRed",
--- "RainbowYellow",
--- "RainbowBlue",
--- "RainbowOrange",
--- "RainbowGreen",
--- "RainbowViolet",
--- "RainbowCyan",
--- }
--- local hooks = require "ibl.hooks"
--- -- create the highlight groups in the highlight setup hook, so they are reset
--- -- every time the colorscheme changes
--- hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
--- vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
--- vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
--- vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
--- vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
--- vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
--- vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
--- vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
--- end)
 
--- vim.g.rainbow_delimiters = { highlight = highlight }
--- require("ibl").setup { scope = { highlight = highlight } }
+-- require("ibl").setup({
+  -- scope = { enabled = false },
+-- })
 
--- hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
---
--- vim.cmd [[highlight IndentBlanklineIndent1 guifg=#35353a gui=nocombine]]
--- vim.cmd [[highlight IndentBlanklineIndent2 guifg=#35353a gui=nocombine]]
--- vim.cmd [[highlight IndentBlanklineIndent3 guifg=#35353a gui=nocombine]]
--- vim.cmd [[highlight IndentBlanklineIndent4 guifg=#35353a gui=nocombine]]
--- vim.cmd [[highlight IndentBlanklineIndent5 guifg=#35353a gui=nocombine]]
--- vim.cmd [[highlight IndentBlanklineIndent6 guifg=#35353a gui=nocombine]]
+local highlight = {
+    "RainbowRed",
+    "RainbowYellow",
+    "RainbowBlue",
+    "RainbowOrange",
+    "RainbowGreen",
+    "RainbowViolet",
+    "RainbowCyan",
+}
+local hooks = require "ibl.hooks"
+-- create the highlight groups in the highlight setup hook, so they are reset
+-- every time the colorscheme changes
+hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+    vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+    vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+    vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+    vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+    vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+    vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+    vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+end)
 
--- require("ibl").setup {
--- char = "▏",
--- buftype_exclude = {"terminal"},
--- show_trailing_blankline_indent = false,
--- show_current_context = true,
--- filetype_exclude = {"help", "terminal"},
--- -- default : {'class', 'function', 'method'}
--- space_char_blankline = " ",
--- char_highlight_list = {
--- "IndentBlanklineIndent1",
--- "IndentBlanklineIndent2",
--- "IndentBlanklineIndent3",
--- "IndentBlanklineIndent4",
--- "IndentBlanklineIndent5",
--- "IndentBlanklineIndent6",
--- },
--- context_patterns = {
--- "class",
--- "function",
--- "method",
--- "^if",
--- "^while",
--- "^for",
--- "^object",
--- "^table",
--- "^type",
--- "^import",
--- "block",
--- "arguments"
--- }
--- -- disabled now for performance hit.
--- -- use_treesitter = true
--- }
+vim.g.rainbow_delimiters = { highlight = highlight }
+require("ibl").setup { scope = { highlight = highlight } }
 
-require("ibl").setup({
-  scope = { enabled = false },
-})
+hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
+
 
 require("noice").setup({
   lsp = {
