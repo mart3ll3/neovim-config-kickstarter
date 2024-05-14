0f92f9f1ccbe207c8790175050ee1f295a3899e5
Juraj Rajnoha <juraj.rajnoha@revalizesoftware.com>
2024-05-14 21:01:11 +0200

    added nvim-java

--- init.lua
+++ init.lua
@@ -463,6 +463,39 @@ require('lazy').setup({
             'nvim-tree/nvim-web-devicons',     -- optional
         }
     },
+{
+  "nvim-java/nvim-java",
+  dependencies = {
+    "nvim-java/lua-async-await",
+    "nvim-java/nvim-java-refactor",
+    "nvim-java/nvim-java-core",
+    "nvim-java/nvim-java-test",
+    "nvim-java/nvim-java-dap",
+    "MunifTanjim/nui.nvim",
+    "neovim/nvim-lspconfig",
+    "mfussenegger/nvim-dap",
+    {
+      "williamboman/mason.nvim",
+      opts = {
+        registries = {
+          "github:nvim-java/mason-registry",
+          "github:mason-org/mason-registry",
+        },
+      },
+    },
+    {
+      "williamboman/mason-lspconfig.nvim",
+      opts = {
+        handlers = {
+          ["jdtls"] = function()
+            require("java").setup()
+          end,
+        },
+      },
+    },
+  },
+  opts = {},
+},
 
     -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
     --       These are some example plugins that I've included in the kickstart repository.
