db57920bbd16a4487fd00aa272975e757a476954
Juraj Rajnoha <juraj.rajnoha@revalizesoftware.com>
2024-05-15 19:45:15 +0200

    disabled bigfile by default

--- init.lua
+++ init.lua
@@ -358,10 +358,10 @@ require('lazy').setup({
         "kdheepak/lazygit.nvim",
         event = "VeryLazy",
     },
-    {
-        "LunarVim/bigfile.nvim",
-        event = "VeryLazy",
-    },
+    -- {
+        -- "LunarVim/bigfile.nvim",
+        -- event = "VeryLazy",
+    -- },
     {
         "folke/trouble.nvim",
         event = "VeryLazy",
@@ -1274,28 +1274,28 @@ require("nvim-ts-autotag").setup()
 -- vim: ts=2 sts=2 sw=2 et
 --
 --
-require("bigfile").setup {
-    -- filesize = 2, -- size of the file in MiB, the plugin round file sizes to the closest MiB
-    pattern = function(bufnr, filesize_mib)
-        -- you can't use `nvim_buf_line_count` because this runs on BufReadPre
-        local file_contents = vim.fn.readfile(vim.api.nvim_buf_get_name(bufnr))
-        local file_length = #file_contents
-        local filetype = vim.filetype.match({ buf = bufnr })
-        if file_length > 5000 and filetype == "cs" then
-            return true
-        end
-    end,
-    features = { -- features to disable
-        "indent_blankline",
-        "illuminate",
-        -- "lsp",
-        -- "treesitter",
-        -- "syntax",
-        -- "matchparen",
-        -- "vimopts",
-        -- "filetype",
-    },
-}
+-- require("bigfile").setup {
+    -- -- filesize = 2, -- size of the file in MiB, the plugin round file sizes to the closest MiB
+    -- pattern = function(bufnr, filesize_mib)
+        -- -- you can't use `nvim_buf_line_count` because this runs on BufReadPre
+        -- local file_contents = vim.fn.readfile(vim.api.nvim_buf_get_name(bufnr))
+        -- local file_length = #file_contents
+        -- local filetype = vim.filetype.match({ buf = bufnr })
+        -- if file_length > 5000 and filetype == "cs" then
+            -- return true
+        -- end
+    -- end,
+    -- features = { -- features to disable
+        -- "indent_blankline",
+        -- "illuminate",
+        -- -- "lsp",
+        -- -- "treesitter",
+        -- -- "syntax",
+        -- -- "matchparen",
+        -- -- "vimopts",
+        -- -- "filetype",
+    -- },
+-- }
 
 -- Using before and after.
 require("themery").setup({
